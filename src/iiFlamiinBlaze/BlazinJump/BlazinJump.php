68f841295d753360e19ca271331c266aff166938
iiFlamiinBlaze <iiflamiinblaze123@gmail.com>
2022-09-20 16:10:06 -0400

    Update to PM 4.0

--- src/iiFlamiinBlaze/BlazinJump/BlazinJump.php
+++ src/iiFlamiinBlaze/BlazinJump/BlazinJump.php
@@ -25,21 +25,21 @@ use pocketmine\entity\Entity;
 use pocketmine\event\Listener;
 use pocketmine\event\player\PlayerJoinEvent;
 use pocketmine\event\player\PlayerJumpEvent;
-use pocketmine\event\player\PlayerPreLoginEvent;
-use pocketmine\Player;
+use pocketmine\event\player\PlayerLoginEvent;
+use pocketmine\event\player\PlayerQuitEvent;
+use pocketmine\player\Player;
 use pocketmine\plugin\PluginBase;
+use pocketmine\scheduler\ClosureTask;
 use pocketmine\utils\TextFormat;
 
 class BlazinJump extends PluginBase implements Listener{
-
-	private const VERSION = "v1.0.3";
+	
+	private const VERSION = "v1.1.0";
 	private const PREFIX = TextFormat::AQUA . "BlazinJump" . TextFormat::GOLD . " > ";
-
-	/** @var array $jumps */
-	public $jumps = [];
-	/** @var self $instance */
-	private static $instance;
-
+	
+	public array $jumps = [];
+	private static self $instance;
+	
 	public function onEnable() : void{
 		self::$instance = $this;
 		$this->getServer()->getPluginManager()->registerEvents($this, $this);
@@ -47,39 +47,44 @@ class BlazinJump extends PluginBase implements Listener{
 		$this->saveDefaultConfig();
 		$this->getLogger()->info("BlazinJump " . self::VERSION . " by BlazeTheDev is enabled");
 	}
-
+	
+	public function onLogin(PlayerLoginEvent $event) : void{
+		$this->jumps[$event->getPlayer()->getName()] = 0;
+	}
+	
+	public function onJoin(PlayerJoinEvent $event) : void{
+		$this->multiWorldCheck($event->getPlayer());
+	}
+	
+	public function onQuit(PlayerQuitEvent $event) : void{
+		if(isset($this->jumps[$event->getPlayer()->getName()])) unset($this->jumps[$event->getPlayer()->getName()]);
+	}
+	
+	public function onJump(PlayerJumpEvent $event) : bool{
+		$player = $event->getPlayer();
+		$this->jumps[$player->getName()]++;
+		if($this->jumps[$player->getName()] === 1) $this->getScheduler()->scheduleDelayedTask(new ClosureTask(function () use ($player) : void{
+			$this->jumps[$player->getName()] = 0;
+		}), 30);
+		if($this->jumps[$player->getName()] === 2){
+			if($this->multiWorldCheck($player) === false) return false;
+			$player->knockBack($player->getDirectionVector()->getX(), $player->getDirectionVector()->getZ(), (float)$this->getConfig()->get("jump-power"));
+			$this->jumps[$player->getName()] = 0;
+		}
+		return true;
+	}
+	
 	private function multiWorldCheck(Entity $entity) : bool{
 		if(!$entity instanceof Player) return false;
 		if($this->getConfig()->get("multi-world") === "on"){
-			if(!in_array($entity->getLevel()->getName(), $this->getConfig()->get("worlds"))){
+			if(!in_array($entity->getWorld()->getDisplayName(), $this->getConfig()->get("worlds"))){
 				$entity->sendMessage(self::PREFIX . TextFormat::RED . "You are not in the right world to be able to double jump");
 				return false;
 			}
 		}elseif($this->getConfig()->get("multi-world") === "off") return true;
 		return true;
 	}
-
-	public function onJoin(PlayerJoinEvent $event) : void{
-		$this->multiWorldCheck($event->getPlayer());
-	}
-
-	public function onPreLogin(PlayerPreLoginEvent $event) : void{
-		$player = $event->getPlayer();
-		$this->jumps[$player->getName()] = 0;
-	}
-
-	public function onJump(PlayerJumpEvent $event) : bool{
-		$player = $event->getPlayer();
-		$this->jumps[$player->getName()]++;
-		if($this->jumps[$player->getName()] === 1) $this->getScheduler()->scheduleDelayedTask(new BlazinJumpTask($player), 30);
-		if($this->jumps[$player->getName()] === 2){
-			if($this->multiWorldCheck($player) === false) return false;
-			$player->knockBack($player, 0, $player->getDirectionVector()->getX(), $player->getDirectionVector()->getZ(), (int)$this->getConfig()->get("jump-power"));
-			$this->jumps[$player->getName()] = 0;
-		}
-		return true;
-	}
-
+	
 	public static function getInstance() : self{
 		return self::$instance;
 	}
